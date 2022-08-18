9e6689792409c4196ef7165b681a329359b63bf9
Blinov Evgeny <sharppoint@yandex.ru>
2022-08-18 11:42:38 +0300

    вынес маршруты в отдельный модуль

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import fetch from "node-fetch";
-//--------You IMPORT---------//
-import {
-  authPath,
-  getAccessToken,
-  user_get,
-  friends_get,
-} from "./modules/vk_paths.js";
-import { OAuthModel, UserModel } from "./modules/models.js";
-//-----------------------//
+import { router as vk_oauth_router } from "./routes/vk_oauth_router.js"
 
-//----- You VARIABLES ----//
-let oauth_model = undefined; // объект хранящий данные авторизации
-let user_model = undefined; // модель авторизованного пользователя
-let req_data_field = "bdate, online, photo_max_orig, sex, counters"; // запрашиваемые у VK API поля
-//------------------------//
 dotenv.config();
 const __dirname = path.resolve();
 const port = process.env.PORT || 1234;
@@ -27,56 +13,12 @@ app.set("views", path.resolve(__dirname, "templates"));
 
 app.use(express.static(path.resolve(__dirname, "static")));
 
-app.get("/", async (req, res) => {
-  /*
-        Если пользователь не авторизован отдать шаблон с кнопкой авторизации,
-        иначе отобразить шаблон с данными пользователя
-    */
-  if (oauth_model !== undefined) {
-    // запрос данных авторизованного пользователя
-    await fetch(
-      // Запросить данные авторизованного пользователя
-      user_get(oauth_model.user_id, oauth_model.access_token, req_data_field) // запрос VK API данных
-    )
-      .then((res) => res.json()) // забрать данные в формате json
-      .then((data) => (user_model = UserModel.fromJson(data.response[0]))) // создать объект пользователя
-      .catch((err) => console.log("Error: " + err)); // обработать ошибку
+// подключение роутинга
+app.use("/vk_oauth", vk_oauth_router)
 
-    res.render("index", { auth: oauth_model, user: user_model }); // шаблон с данными пользователя
-  } else {
-    res.render("index", { auth: oauth_model, auth_path: "/auth" }); // шаблон с кнопкой авторизации
-  }
-});
 
-app.get("/auth", (req, res) => {
-  /* Запрос на получение кода авторизации */
-  res.redirect(
-    authPath(
-      process.env.APPID,
-      "http://localhost:1234/oauth_redirect",
-      "friends, photos"
-    )
-  );
-});
-
-app.get("/oauth_redirect", async (req, res) => {
-    /* Если код авторизации получен , запрашиваем access_token и переходи на корневой узел*/
-  const code = req.query["code"];
-  if (code) {
-    await fetch(
-      getAccessToken(
-        process.env.APPID,
-        "http://localhost:1234/oauth_redirect",
-        process.env.SECRET,
-        code
-      )
-    )
-      .then((res) => res.json())
-      .then((json) => (oauth_model = OAuthModel.fromJson(json))) // создаём модель с данными авторизации
-      .catch((err) => console.log("Err: " + err));
-
-    res.redirect("/");
-  }
+app.get("/", async (req, res) => {  
+  res.render("index"); // шаблон с кнопкой авторизации  
 });
 
 app.listen(port, () => {
---
+++ routes/vk_oauth_router.js
@@ -0,0 +1,74 @@
+import express from "express";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+
+import {
+  authPath,
+  getAccessToken,
+  user_get,
+  friends_get,
+} from "../modules/vk_paths.js"//"../vk_paths.js";
+import { OAuthModel, UserModel } from "../modules/models.js" //"../models.js";
+
+dotenv.config();
+
+//----- You VARIABLES ----//
+let oauth_model = undefined; // объект хранящий данные авторизации
+let user_model = undefined; // модель авторизованного пользователя
+let req_data_field = "bdate, online, photo_max_orig, sex, counters"; // запрашиваемые у VK API поля
+//------------------------//
+
+let router = express.Router();
+
+router.get("/", (req, res) => {
+    //------------------------------------
+  /* Запрос на получение кода авторизации */
+  res.redirect(
+    authPath(
+      process.env.APPID,
+      "http://localhost:1234/vk_oauth/token",
+      "friends, photos"
+    )
+  );
+  //--------------------------------------
+});
+
+router.get("/token", async (req, res) => {
+    //----------------------------------------------------
+  /* Если код авторизации получен , запрашиваем access_token и переходи на корневой узел*/
+  const code = req.query["code"];
+  if (code) {
+    await fetch(
+      getAccessToken(
+        process.env.APPID,
+        "http://localhost:1234/vk_oauth/token",
+        process.env.SECRET,
+        code
+      )
+    )
+      .then((res) => res.json())
+      .then((json) => (oauth_model = OAuthModel.fromJson(json)))
+      .catch((err) => console.log("Err: " + err));
+
+    res.redirect("/vk_oauth/access");
+  }
+  //------------------------------------------------
+});
+
+router.get("/access", async (req, res) => {
+  //----------------------------------
+  if (oauth_model !== undefined) {
+    await fetch(
+      // Запросить данные авторизованного пользователя
+      user_get(oauth_model.user_id, oauth_model.access_token, req_data_field)
+    )
+      .then((res) => res.json())
+      .then((data) => (user_model = UserModel.fromJson(data.response[0]))) 
+      .catch((err) => console.log("Error: " + err));
+
+    res.send("user:ok")
+  } 
+  //--------------------------------
+});
+
+export { router };
